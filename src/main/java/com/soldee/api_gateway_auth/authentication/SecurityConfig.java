8e21c96c3235047f950db4f26844ef0d92e74a79
soldee <71665509+soldee@users.noreply.github.com>
2023-06-09 19:59:22 +0200

    x509 client certificate validation and authentication based on CN extraction

--- src/main/java/com/soldee/api_gateway_auth/authentication/SecurityConfig.java
+++ src/main/java/com/soldee/api_gateway_auth/authentication/SecurityConfig.java
@@ -1,21 +1,32 @@
 package com.soldee.api_gateway_auth.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.X509Configurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 import java.security.cert.CertificateParsingException;
+import java.security.cert.X509Certificate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @EnableWebSecurity
 @Component
 public class SecurityConfig {
+
+    Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,33 +35,50 @@ public class SecurityConfig {
                     matcherRegistry
                             .anyRequest().authenticated();
                 })
-                .x509(x509Configurer -> {
-                    x509Configurer
-                            .x509PrincipalExtractor(cert -> {
-                                System.out.println(cert.getIssuerX500Principal());
-                                System.out.println(cert.getSubjectDN().getName());
-                                System.out.println(cert.getSubjectX500Principal().getName());
-                                System.out.println(cert.getSerialNumber());
-                                try {
-                                    cert.getSubjectAlternativeNames().forEach(System.out::println);
-                                } catch (CertificateParsingException e) {
-                                    e.printStackTrace();
-                                }
-                                return "test";
-                            })
-                            .userDetailsService(username -> {
-                                if (username.equals("test")) {
-                                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
-                                }
-                                throw new UsernameNotFoundException("Invalid certificate");
-                            });
-                })
+                .x509(this::x509Config)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionConfigurer -> {
                     sessionConfigurer
                             .sessionCreationPolicy(SessionCreationPolicy.NEVER);
                 })
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+
+    private void x509Config(X509Configurer<HttpSecurity> x509Configurer) {
+        x509Configurer
+                .x509PrincipalExtractor(this::extractCN)
+                .userDetailsService(this::authenticateUser);
+    }
+
+
+    private String extractCN(X509Certificate x509Cert) {
+        String certificateInfo = x509Cert.getSubjectDN().getName();
+
+        log.debug("Extracting certificate info..." + certificateInfo);
+
+        Pattern subjectCNPattern = Pattern.compile("CN=(.*?)(?:, )", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = subjectCNPattern.matcher(certificateInfo);
+
+        if (!matcher.find()) throw new UsernameNotFoundException("Invalid certificate");
+
+        try {
+            String cn = matcher.group(1);
+            log.info("Extracted CN=" + cn);
+
+            return cn;
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Invalid certificate");
+        }
+    }
+
+
+    private User authenticateUser(String username) {
+        if (username.equals("bob")) {
+            return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        }
+        throw new UsernameNotFoundException("Invalid certificate");
     }
 
 }
